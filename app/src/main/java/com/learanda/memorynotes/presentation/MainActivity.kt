c6a95b6755f4176c643868bd0edd155a304e5477
learanda <learanda@hotmail.com.ar>
2024-03-24 15:01:20 -0300

    Add navigation

--- app/src/main/java/com/learanda/memorynotes/presentation/MainActivity.kt
+++ app/src/main/java/com/learanda/memorynotes/presentation/MainActivity.kt
@@ -3,6 +3,8 @@ package com.learanda.memorynotes.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,8 +12,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.learanda.memorynotes.R
 import com.learanda.memorynotes.ui.theme.MemoryNotesTheme
 
+/*
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,5 +45,18 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     MemoryNotesTheme {
         Greeting("Android")
+    }
+}*/
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
--- app/src/main/java/com/learanda/memorynotes/presentation/NoteFragment.kt
+++ app/src/main/java/com/learanda/memorynotes/presentation/NoteFragment.kt
@@ -5,28 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation.findNavController
+import androidx.navigation.Navigation
 import com.learanda.memorynotes.R
-
-class ListFragment : Fragment() {
+class NoteFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        return inflater.inflate(R.layout.fragment_note, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        this.requireView().findViewById<View>(R.id.addNote).setOnClickListener {
-            goToNoteDetails()
+        this.requireView().findViewById<View>(R.id.checkButton).setOnClickListener {
+            Navigation.findNavController(it).popBackStack()
         }
-    }
-
-    private fun goToNoteDetails(id: Long = 0L) {
-        val action = ListFragmentDirections.actionGoToNote().setNoteId(id)
-        findNavController(this.requireView().findViewById(R.id.notesListView)).navigate(action)
     }
 }
