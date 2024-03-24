67ef2cf8899c803fb54a6c1d98fa3f43cf16479e
learanda <learanda@hotmail.com.ar>
2024-03-24 20:29:09 -0300

    Add room note datasource

---
+++ app/src/main/java/com/learanda/memorynotes/framework/RoomNoteDataSource.kt
@@ -0,0 +1,19 @@
+package com.learanda.memorynotes.framework
+
+import android.content.Context
+import com.learanda.core.data.Note
+import com.learanda.core.repository.NoteDataSource
+import com.learanda.memorynotes.framework.db.DatabaseService
+import com.learanda.memorynotes.framework.db.NoteEntity
+
+class RoomNoteDataSource(context: Context): NoteDataSource {
+    val noteDao = DatabaseService.getInstance(context).noteDao()
+
+    override suspend fun add(note: Note) = noteDao.addNoteEntity(NoteEntity.fromNote(note))
+
+    override suspend fun get(id: Long): Note? = noteDao.getNoteEntity(id)?.toNote()
+
+    override suspend fun getAll(): List<Note> = noteDao.getAllNoteEntities().map { it.toNote() }
+
+    override suspend fun remove(note: Note) = noteDao.deleteNoteEntity(NoteEntity.fromNote(note))
+}
