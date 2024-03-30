c9f15b6fe5b42b59fe8652437d16614ab312a26d
learanda <learanda@hotmail.com.ar>
2024-03-30 12:29:45 -0300

    Add adapter to recyclerview

---
+++ app/src/main/java/com/learanda/memorynotes/presentation/NotesListAdapter.kt
@@ -0,0 +1,46 @@
+package com.learanda.memorynotes.presentation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.learanda.core.data.Note
+import com.learanda.memorynotes.R
+import java.text.SimpleDateFormat
+import java.util.Date
+
+class NotesListAdapter(var notes: ArrayList<Note>): RecyclerView.Adapter<NotesListAdapter.NoteViewHolder>() {
+
+    fun updatesNotes(newNotes: List<Note>) {
+        notes.clear()
+        notes.addAll(notes)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder = NoteViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.item_note, parent, false)
+    )
+
+    override fun getItemCount(): Int = notes.size
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        holder.bind(notes[position])
+    }
+    inner class NoteViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+        private val layout = view.findViewById<CardView>(R.id.noteLayout)
+        private val noteTitle = view.findViewById<TextView>(R.id.title)
+        private val noteContent = view.findViewById<TextView>(R.id.content)
+        private val noteDate = view.findViewById<TextView>(R.id.date)
+        fun bind(note: Note) {
+            noteTitle.text = note.title
+            noteContent.text = note.content
+
+            val sdf = SimpleDateFormat("MMM dd, HH:mm:ss")
+            val resultDate = Date(note.updateTime)
+            noteDate.text = "Last updated: ${sdf.format(resultDate)}"
+        }
+    }
+}
